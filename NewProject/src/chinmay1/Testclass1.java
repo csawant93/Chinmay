671b921c64dcb9bf3aec999b7d05078d6dbd9f8f
csawant93 <110191848+csawant93@users.noreply.github.com>
2022-07-28 23:27:41 +0530

    test1
    Hi ,
    i have perform the task.

---
+++ NewProject/src/chinmay1/Testclass1.java
@@ -0,0 +1,11 @@
+package chinmay1;
+
+public class Testclass1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Project updated");
+
+	}
+
+}
---
+++ NewProject/src/groupX/Testclass.java
@@ -0,0 +1,9 @@
+package groupX;
+
+public class Testclass {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Project Created");
+	}
+}
